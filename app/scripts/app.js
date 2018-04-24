14c2f1b286118a0b0820a8e1cfc848f48ece6d18
Kasper Pickett <kasperpickett@gmail.com>
2018-04-24 01:33:29 -0500

    assignment 3 routing/states in progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,13 +11,18 @@
       .state('landing', {
           url: '/',
           templateUrl: '/templates/landing.html'
-          })
+        })
 
       .state('album', {
         url: '/album',
         templateUrl: '/templates/album.html'
+      })
+
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
       });
-      
+
     angular
       .module('blocJams', ['ui.router'])
       .config(config);
