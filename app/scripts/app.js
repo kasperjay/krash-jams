bdac1a0c87a5a80dd126b28159953467ce9cd1ab
Kasper Pickett <kasperpickett@gmail.com>
2018-04-26 21:09:49 -0500

    assignment 3 routing & states completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,23 +5,23 @@
           enabled: true,
           requireBase: false
         });
-    }
 
-    $stateProvider
-      .state('landing', {
+      $stateProvider
+        .state('landing', {
           url: '/',
           templateUrl: '/templates/landing.html'
         })
 
-      .state('album', {
-        url: '/album',
-        templateUrl: '/templates/album.html'
-      })
+        .state('album', {
+          url: '/album',
+          templateUrl: '/templates/album.html'
+        })
 
-      .state('collection', {
-        url: '/collection',
-        templateUrl: '/templates/collection.html'
-      });
+        .state('collection', {
+          url: '/collection',
+          templateUrl: '/templates/collection.html'
+        });
+      }
 
     angular
       .module('blocJams', ['ui.router'])
